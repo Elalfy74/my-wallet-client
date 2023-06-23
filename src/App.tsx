13beb6778c986abf38f31a6de858d44330be1937
elalfy74 <mahmoudelalfy13@gmail.com>
2023-06-23 22:03:41 +0300

    fixed build

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ThemeProvider } from './ThemeProvider';
-import { Login, Register } from './pages/auth';
-import { Home } from './pages/home/page';
+
+import { MainLayout } from './layout';
 import ProtectedRoute from './protected-route';
 import PublicRoute from './public-route';
-import { MainLayout } from './layout';
-import { Wallet } from './pages/wallet/page';
-import { NewTransaction } from './pages/new-transaction/page';
+
+import { Login, Register } from './pages/auth';
+import { Home } from './pages/home';
+import { Wallet } from './pages/wallet';
+import { NewTransaction } from './pages/new-transaction';
 
 const router = createBrowserRouter([
   {
