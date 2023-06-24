0e82e51396eb7e0a1c985b28a75ad3e030109f2e
elalfy74 <mahmoudelalfy13@gmail.com>
2023-06-24 11:11:42 +0300

    add precision to number input

--- src/pages/new-transaction/components/payment.tsx
+++ src/pages/new-transaction/components/payment.tsx
@@ -31,6 +31,7 @@ export function Payment({
       >
         <NumberInput
           placeholder="Amount"
+          precision={2}
           hideControls
           name="amount"
           w="80%"
